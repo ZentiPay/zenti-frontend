edef7bfcbeeac41d7e7ab46727dc9c71ff71b5ba
prathibhabilagi <prathibhabilagi12@gmail.com>
2025-06-01 23:08:23 +0200

    header navbar

---
+++ src/components/Header.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React, { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Product", href: "#product" },
+  { name: "Features", href: "#features" },
+  { name: "Contact", href: "#contact" },
+];
+
+const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <header className="sticky top-0 inset-x-0 z-50 mb-6 bg-white">
+      <nav className="flex items-center justify-between py-4 lg:py-6">
+        <div className="flex lg:flex-1">
+          <a href="/">
+            <span className="sr-only">Zenti pay</span>
+            <img className="h-6 w-12" src="/logo.png" alt="Zenti Pay" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="cursor-pointer text-sm font-medium font-stretch-expanded text-light-text-primary"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="/">
+              <span className="sr-only">Zenti pay</span>
+              <img className="h-6 w-12" src="/logo.png" alt="Zenti Pay" />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-light-text-primary hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  );
+};
+
+export default Header;
